bca9c3146db0e77f627b1b15a3ff1a75114d7908
mdYaqin <abu_naal@hotmail.com>
2021-09-05 15:59:40 +0800

    Minor changes in naming convention

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,9 +12,9 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-  title: 'Welcome To ProShop',
-  description: 'We sell the best products for cheap',
-  keywords: 'electronics, buy electronics, cheap electroincs',
+  title: 'Welcome To The Artsy',
+  description: 'We sell the custom made stickers and Keychains',
+  keywords: 'custom design, Stickers, Keychains',
 }
 
 export default Meta
