4dcba86e97e28d89272eade5ba4a164b31c60ecf
mdYaqin <abu_naal@hotmail.com>
2021-09-05 15:53:16 +0800

    updated store name

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>The Artsy</Navbar.Brand>
+            <Navbar.Brand>The Artsy.Co</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
